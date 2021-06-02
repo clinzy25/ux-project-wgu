9b65d1359450dfcf64e79d9877dfa5635d923f6c
clinzy25 <conner.linzy25@gmail.com>
2021-06-02 11:17:55 -0400

    chat dropdown link

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const chatBtn = document.querySelector('.chat-btn');
+const dropdownChatBtn = document.querySelector('.dropdown-chat');
 const chatWindow = document.querySelector('.page-content');
 
 chatBtn.addEventListener('click', () => {
     chatWindow.classList.toggle('d-none');
 });
+dropdownChatBtn.addEventListener('click', () => {
+    chatWindow.classList.toggle('d-none');
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+
