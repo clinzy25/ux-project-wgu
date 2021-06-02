47ff56cd434e3bf1610b5cea5514bf5f74e22159
clinzy25 <conner.linzy25@gmail.com>
2021-06-02 10:28:43 -0400

    prototype final

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const chatBtn = document.querySelector('.chat-btn');
+const chatWindow = document.querySelector('.page-content');
 
+chatBtn.addEventListener('click', () => {
+    chatWindow.classList.toggle('d-none');
+});
